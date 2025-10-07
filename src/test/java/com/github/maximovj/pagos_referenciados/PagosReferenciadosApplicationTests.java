866569f8e9e299a3053e0932066e08e43c6cb507
Víctor J. <maximovj@outlook.com>
2025-10-07 15:22:42 -0600

    feature: Se agrega DTO para response

---
+++ src/test/java/com/github/maximovj/pagos_referenciados/PagosReferenciadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maximovj.pagos_referenciados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosReferenciadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
