726035fcdedd305012fd959d3cfc6b668b3994f5
Víctor J. <maximovj@outlook.com>
2025-10-07 15:22:22 -0600

    feature: Se agrega DTO para usarlos como request  (consulta)

---
+++ src/main/java/com/github/maximovj/pagos_referenciados/request/PaymentRequest.java
@@ -0,0 +1,26 @@
+package com.github.maximovj.pagos_referenciados.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Data
+public class PaymentRequest {
+
+    @NotNull(message = "El monto es obligatorio")
+    @Positive(message = "El monto debe ser mayor a cero")
+    private Double amount;
+
+    @NotBlank(message = "La descripción es obligatoria")
+    private String description;
+
+    @NotBlank(message = "La fecha de vencimiento es obligatoria")
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", message = "Formato de fecha inválido: yyyy-MM-dd HH:mm:ss")
+    private String dueDate;
+
+    private String callbackURL;
+
+    private String externalId;
+}
